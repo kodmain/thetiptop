f242d1a2d6c77ed14b1e2600c2b47fdf56db4a8e
Florent <contact@making.codes>
2024-03-29 01:24:45 +0100

    fix: app section

--- deploy/aws/app/main.tf
+++ deploy/aws/app/main.tf
@@ -26,7 +26,7 @@ resource "aws_acm_certificate" "cert" {
   provider          = aws.global
   domain_name       = "kodmain.run"
   validation_method = "DNS"
-
+  subject_alternative_names = ["staging.kodmain.run"] 
   tags = {
     Environment = "production"
   }
@@ -35,7 +35,23 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-# Attendez la validation du certificat avant de créer la distribution CloudFront
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.app.id  // Assurez-vous que cela correspond au nom de votre bucket S3
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = ["s3:GetObject"]
+        Effect    = "Allow"
+        Principal = "*"
+        Resource  = ["arn:aws:s3:::${aws_s3_bucket.app.bucket}/*"]
+      }
+    ]
+  })
+}
+
+
 resource "aws_acm_certificate_validation" "cert_validation" {
   provider        = aws.global
   certificate_arn = aws_acm_certificate.cert.arn
@@ -59,218 +75,6 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 
-variable "github_token" {
-  description = "GitHub token"
-}
-
-resource "random_password" "password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
-
-resource "aws_instance" "free_tier_arm_instance" {
-  # X86 ami-089c89a80285075f7 t2.micro  Amazon Linux 2 # WORKING
-  # ARM ami-09e82d7942ffb02d3 t4g.micro Amazon Linux 2 # NOT WORKING (fixed by client.cpu_total_compute in nomad-server.hcl)
-  ami           = "ami-09e82d7942ffb02d3"
-  instance_type = "t4g.micro"
-  associate_public_ip_address = false
-
-  tags = {
-    Name = "NomadServer"
-  }
-  
-  user_data = <<-EOF
-    #!/bin/bash
-    yum install -y yum-utils
-    yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-    yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-    yum -y install nomad docker gh cni-plugins httpd-tools htop
-    git clone https://github.com/kodmain/thetiptop /home/ec2-user/thetiptop
-    cp /home/ec2-user/thetiptop/deploy/server/nomad.service /etc/systemd/system/nomad.service
-    sleep 1
-    systemctl enable nomad 
-    systemctl enable docker
-    systemctl start docker
-    systemctl start nomad
-    sleep 1
-    nomad acl bootstrap > /home/ec2-user/bootstrap.token
-    export NOMAD_TOKEN=$(cat /home/ec2-user/bootstrap.token | grep "Secret" |awk '{print $4}')
-    export GH_TOKEN="${var.github_token}"
-    export GF_ADMIN_PASSWORD="${random_password.password.result}"
-    echo "export NOMAD_TOKEN=$NOMAD_TOKEN" >> /home/ec2-user/.bashrc
-    echo "export GH_TOKEN='$GH_TOKEN'" >> /home/ec2-user/.bashrc
-    echo "export GF_ADMIN_PASSWORD='$GF_ADMIN_PASSWORD'" >> /home/ec2-user/.bashrc
-    nomad acl policy apply -description "Deployment" deploy /home/ec2-user/thetiptop/deploy/server/nomad-policy.hcl
-    nomad acl token create -name="github" -policy="deploy" > /home/ec2-user/github.token
-    export GITHUB_NOMAD_TOKEN=$(cat /home/ec2-user/github.token | grep "Secret" |awk '{print $4}')
-    gh secret set NOMAD_TOKEN -b"$GITHUB_NOMAD_TOKEN" --repo kodmain/thetiptop
-    sed -i 's/NOMADTOKEN/'"$NOMAD_TOKEN"'/g' /home/ec2-user/thetiptop/deploy/aws/api/jobs/server.hcl
-    sleep 1
-    nomad job run -token=$NOMAD_TOKEN /home/ec2-user/thetiptop/deploy/aws/api/jobs/server.hcl
-    nomad job run -token=$NOMAD_TOKEN -var="grafana_admin_password=$GF_ADMIN_PASSWORD" /home/ec2-user/thetiptop/deploy/aws/api/jobs/middlewares.hcl
-  EOF
-
-  iam_instance_profile = aws_iam_instance_profile.traefik_instance_profile.name
-
-  root_block_device {
-    volume_size = 10
-    volume_type = "gp3"
-  }
-  
-  security_groups = [aws_security_group.nomad.name]
-  key_name = aws_key_pair.remote.key_name 
-}
-
-resource "aws_key_pair" "remote" {
-  key_name   = "kodmain"
-  public_key = file("~/.ssh/kodmain.pub")
-}
-
-resource "aws_security_group" "nomad" {
-  name        = "nomad"
-  description = "Security Group for Nomad Server"
-
-  /* Disable use nomad.kodmain.run
-  ingress {
-    from_port   = 4646
-    to_port     = 4646
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   */
-
-  /* Disable use traefik.kodmain.run
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  */
-
-  /* Disable SSH 
-  */
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_iam_role" "traefik_route53_role" {
-  name = "TraefikRoute53Role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "traefik_route53_policy" {
-  name = "TraefikRoute53Policy"
-  role = aws_iam_role.traefik_route53_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "ssm:UpdateInstanceInformation",
-          "route53:GetChange",
-          "route53:ChangeResourceRecordSets",
-          "route53:ListResourceRecordSets",
-          "route53:ListHostedZones",
-          "route53:ListHostedZonesByName"
-        ],
-        Effect = "Allow",
-        Resource = "*"
-      },
-      {
-        Action: [
-          "cloudwatch:ListMetrics",
-          "cloudwatch:GetMetricData",
-          "cloudwatch:GetMetricStatistics",
-          "cloudwatch:DescribeAlarms",
-          "cloudwatch:DescribeAlarmHistory",
-          "cloudwatch:DescribeAlarmsForMetric"
-        ],
-        Effect: "Allow",
-        Resource: "*"
-      },
-      {
-        Action = [
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:GetLogEvents",
-          "logs:FilterLogEvents"
-        ],
-        Effect = "Allow",
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_instance_profile" "traefik_instance_profile" {
-  name = "TraefikInstanceProfile"
-  role = aws_iam_role.traefik_route53_role.name
-}
-
-resource "aws_route53_record" "kodmain_wildcard" {
-  zone_id = "Z10052173VRSYMBUSS942"
-
-  name    = "*.kodmain.run"  # Enregistrement wildcard pour tous les sous-domaines
-  type    = "A"
-  ttl     = 10
-  records = [aws_instance.free_tier_arm_instance.public_ip]
-
-  allow_overwrite = true
-}
-
-resource "aws_route53_record" "kodmain_internal" {
-  zone_id = "Z10052173VRSYMBUSS942"
-
-  name    = "internal.kodmain.run"  # Enregistrement wildcard pour tous les sous-domaines
-  type    = "A"
-  ttl     = 10
-  records = [aws_instance.free_tier_arm_instance.private_ip]
-
-  allow_overwrite = true
-}
-
-
 resource "aws_s3_bucket" "app" {
   bucket = "kodmain"
   force_destroy = true
@@ -285,58 +89,36 @@ resource "aws_s3_bucket_public_access_block" "app_public_access_block" {
   restrict_public_buckets = false
 }
 
+resource "aws_cloudfront_function" "path_rewrite_function" {
+  name    = "PathRewriteFunction"
+  runtime = "cloudfront-js-1.0"
 
-resource "aws_s3_bucket_public_access_block" "logs_public_access_block" {
-  bucket = aws_s3_bucket.logs.id
+  publish = true
 
-  block_public_acls       = false
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
+  code = <<-EOT
+    function handler(event) {
+    var request = event.request;
+    var uri = request.uri;
+    var headers = request.headers;
+    var host = headers.host.value;
+
+    // Rediriger en fonction du domaine
+    if (host === 'staging.kodmain.run') {
+        uri = '/staging' + uri;
+    } else if (host === 'kodmain.run') {
+        uri = '/production' + uri;
+    }
+
+    // Rediriger vers un document par défaut si nécessaire
+    if (uri.endsWith('/') || !/\.[^/]+$/.test(uri)) {
+        uri = uri.endsWith('/') ? uri + 'index.html' : uri + '/index.html';
+    }
+
+    request.uri = uri;
+    return request;
 }
 
-
-resource "aws_s3_access_point" "kodmain_access_point" {
-  name         = "kodmain"
-  bucket       = aws_s3_bucket.app.id
-
-  public_access_block_configuration {
-    block_public_acls       = false
-    block_public_policy     = false
-    ignore_public_acls      = false
-    restrict_public_buckets = false
-  }
-}
-
-resource "aws_s3_bucket_policy" "bucket_log_policy" {
-  bucket = aws_s3_bucket.logs.id
-
-   policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "logs_policy"
-    Statement = [
-      {
-        Sid       = "HTTPSOnly"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource = [
-          aws_s3_bucket.logs.arn,
-          "${aws_s3_bucket.logs.arn}/*",
-        ]
-        Condition = {
-          Bool = {
-            "aws:SecureTransport" = "false"
-          }
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_s3_bucket" "logs" {
-  bucket = "logs.kodmain"
-  force_destroy = true
+  EOT
 }
 
 
@@ -354,9 +136,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     Environment = "production"
   }
 
-  aliases = ["kodmain.run"]
+  aliases = ["kodmain.run","staging.kodmain.run"]
 
   default_cache_behavior {
+    function_association {
+      event_type = "viewer-request"
+      function_arn = aws_cloudfront_function.path_rewrite_function.arn
+    }
+    
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = "S3-${aws_s3_bucket.app.id}"
@@ -364,6 +151,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     forwarded_values {
       query_string = true
+      headers      = ["Origin"]
       cookies {
         forward = "all"
       }
@@ -386,40 +174,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       restriction_type = "none"
     }
   }
-  
-  logging_config {
-    include_cookies = false
-    bucket          = "${aws_s3_bucket.logs.bucket}.s3.amazonaws.com"
-    prefix          = "thetiptop"
-  }
-  
+
 }
 
-resource "aws_s3_bucket_policy" "bucket_app_policy" {
-  bucket = aws_s3_bucket.app.id
-  
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Id      = "AllowGetObjects",
-    Statement = [
-      {
-        Sid       = "HTTPSOnly"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource = [
-          aws_s3_bucket.app.arn,
-          "${aws_s3_bucket.app.arn}/*",
-        ]
-        Condition = {
-          Bool = {
-            "aws:SecureTransport" = "false"
-          }
-        }
-      }
-    ]
-  })
-}
 
 resource "aws_s3_bucket_website_configuration" "project_website" {
   bucket = aws_s3_bucket.app.id
@@ -434,8 +191,21 @@ resource "aws_s3_bucket_website_configuration" "project_website" {
 }
 
 resource "aws_route53_record" "kodmain_cloudfront" {
-  zone_id = "Z10052173VRSYMBUSS942"  # Remplacez par l'ID de votre zone Route 53
+  zone_id = "Z10052173VRSYMBUSS942"
   name    = "kodmain.run"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
+resource "aws_route53_record" "staging_kodmain_cloudfront" {
+  zone_id = "Z10052173VRSYMBUSS942"
+  name    = "staging.kodmain.run"
   type    = "A"
 
   alias {
