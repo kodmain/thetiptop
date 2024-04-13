b6a72b6a9b0b47d1cb8d896acf25fc655c9db969
Kodflow <133899878+kodflow@users.noreply.github.com>
2024-04-13 19:36:56 +0200

    fix: terraform:S6281

--- deploy/aws/api/main.tf
+++ deploy/aws/api/main.tf
@@ -246,6 +246,15 @@ resource "aws_s3_bucket" "config_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "config_bucket_policy_access_block" {
+  bucket = aws_s3_bucket.config_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "config_bucket_policy" {
   bucket = aws_s3_bucket.config_bucket.id
 
