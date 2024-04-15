e2ae3ecd470f9c31259457688ff4026ffd4515eb
Kodflow <133899878+kodflow@users.noreply.github.com>
2024-04-15 21:19:56 +0200

    fix: workflow for gorm
    enable CGO build

--- deploy/aws/api/main.tf
+++ deploy/aws/api/main.tf
@@ -106,14 +106,13 @@ resource "aws_security_group" "nomad" {
   }
   */
 
-  /* Disable SSH 
+  /* Disable SSH */
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  */
 
   ingress {
     from_port   = 80
