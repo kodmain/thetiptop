133f57254494ca409bfbc1e6ce2b4eb84493217f
Gui-D <guillaume.ditte@gmail.com>
2024-05-17 23:45:42 +0200

    signin #248

--- app/lib/src/domain/repositories/token_repository.dart
+++ app/lib/src/domain/repositories/token_repository.dart
@@ -1,39 +1,43 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thetiptop_client/src/domain/env/env.dart';
 import 'package:thetiptop_client/src/domain/models/token.dart';
+import 'package:thetiptop_client/src/infrastructure/providers/shared_preferences_provider.dart';
+import 'package:thetiptop_client/src/infrastructure/services/dio/dio_service_provider.dart';
+import 'package:thetiptop_client/src/infrastructure/services/exception/viewer_exception_provider.dart';
+
+part 'generated/token_repository.g.dart';
 
 class TokenRepository {
+  final Dio dioService;
+  final ViewerExceptionService viewerExceptionService;
   final SharedPreferences sharedPreferences;
-  final Dio httpClient;
 
   TokenRepository({
+    required this.dioService,
+    required this.viewerExceptionService,
     required this.sharedPreferences,
-    required this.httpClient,
   });
 
-  Future<Token> getToken(String email, String password) async {
-    final response = await httpClient.post(
-      '${Env.apiUrl}/sign/in',
-      data: {
-        'email': email,
-        'password': password,
-      },
-    );
-    if (response.statusCode == 200 && response.data != null) {
-      final token = Token.fromJson(json.decode(response.data!) as Map<String, Object?>);
+  Future<Token?> getToken(String email, String password) async {
+    try {
+      FormData formData = FormData();
+      formData.fields.add(MapEntry("email", email));
+      formData.fields.add(MapEntry("password", password));
+      final response = await dioService.post('${Env.apiUrl}/sign/in', data: formData);
+      final token = Token.fromJson(json.decode(response.toString()));
       return token;
-    } else {
-      throw Exception(
-        'getToken ${response.statusCode}, data=${response.data}',
-      );
+    } on DioException catch (error) {
+      viewerExceptionService.showDioError(error);
+      throw Exception(error);
     }
   }
 
   Future<Token> renewToken(Token token) async {
-    final response = await httpClient.post(
+    final response = await dioService.post(
       '${Env.apiUrl}/sign/renew',
       data: {
         'token': token,
@@ -50,4 +54,17 @@ class TokenRepository {
   void saveLocalToken(Token value) {
     sharedPreferences.setString("token", value.toString());
   }
+
+  void removeLocalToken() {
+    sharedPreferences.remove("token");
+  }
+}
+
+@riverpod
+TokenRepository tokenRepository(TokenRepositoryRef ref) {
+  return TokenRepository(
+    dioService: ref.read(dioServiceProvider),
+    viewerExceptionService: ref.read(viewerExceptionServiceProvider.notifier),
+    sharedPreferences: ref.watch(sharedPreferencesProvider),
+  );
 }
