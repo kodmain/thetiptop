0bcdda143240a2ed9e3992cfd6fb3ebf37c43b10
Kodflow <133899878+kodflow@users.noreply.github.com>
2024-07-10 22:26:08 +0200

    test: add more

--- api/internal/infrastructure/providers/mail/mail.go
+++ api/internal/infrastructure/providers/mail/mail.go
@@ -18,6 +18,7 @@ const (
 	mimeVersion              = "MIME-Version"
 	contentType              = "Content-Type"
 	contentTransfertEncoding = "Content-Transfer-Encoding"
+	contentDisposition       = "Content-Disposition"
 )
 
 // Mail Représente un e-mail avec toutes les informations nécessaires pour l'envoi.
@@ -60,6 +61,10 @@ func (m *Mail) IsValid() bool {
 // - []byte: La représentation SMTP de l'e-mail.
 // - error: Une erreur si la construction échoue.
 func (m *Mail) Prepare(service ServiceInterface) ([]byte, []string, error) {
+	if service == nil {
+		return nil, nil, fmt.Errorf("mail service is nil")
+	}
+
 	// Création d'un buffer pour construire le message
 	var msg bytes.Buffer
 	fromHeader := service.From()
@@ -139,7 +144,7 @@ func (m *Mail) addContent(writer *multipart.Writer, ct string, content []byte) e
 
 	header := make(textproto.MIMEHeader)
 	header.Set(contentType, ct+"; charset=UTF-8")
-	header.Set("Content-Transfer-Encoding", "quoted-printable")
+	header.Set(contentTransfertEncoding, "quoted-printable")
 
 	part, err := writer.CreatePart(header)
 	if err != nil {
@@ -166,8 +171,8 @@ func (m *Mail) addAttachment(writer *multipart.Writer, name string, data []byte)
 
 	header := make(textproto.MIMEHeader)
 	header.Set(contentType, "application/octet-stream")
-	header.Set("Content-Transfer-Encoding", "base64")
-	header.Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", name))
+	header.Set(contentTransfertEncoding, "base64")
+	header.Set(contentDisposition, fmt.Sprintf("attachment; filename=\"%s\"", name))
 
 	part, err := writer.CreatePart(header)
 	if err != nil {
--- api/internal/infrastructure/providers/mail/mail_test.go
+++ api/internal/infrastructure/providers/mail/mail_test.go
@@ -1,11 +1,19 @@
 package mail_test
 
-/*
+import (
+	"testing"
+
+	"github.com/kodmain/thetiptop/api/internal/infrastructure/providers/mail"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
 const (
 	GOOD_EMAIL = "user1@example.com"
 	GOOD_PASS  = "ValidP@ssw0rd1"
 )
 
+/*
 func TestMail(t *testing.T) {
 	config.Load(aws.String("../../../../config.test.yml"))
 
@@ -33,3 +41,97 @@ func TestMail(t *testing.T) {
 	assert.NoError(t, err)
 }
 */
+
+// MockService est un mock de ServiceInterface
+type MockService struct {
+	mock.Mock
+}
+
+func (m *MockService) From() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+func (m *MockService) Expeditor() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+func (m *MockService) Send(mail *mail.Mail) error {
+	args := m.Called(mail)
+	return args.Error(0)
+}
+
+func TestMail(t *testing.T) {
+	t.Run("IsValid", func(t *testing.T) {
+		t.Run("ValidMail", func(t *testing.T) {
+			m := &mail.Mail{
+				To:      []string{GOOD_EMAIL},
+				Subject: "Welcome to The Tip Top",
+				Text:    []byte("hello"),
+			}
+
+			assert.True(t, m.IsValid())
+		})
+
+		t.Run("InvalidMail", func(t *testing.T) {
+			m := &mail.Mail{
+				To:      []string{GOOD_EMAIL},
+				Subject: "Welcome to The Tip Top",
+			}
+
+			assert.False(t, m.IsValid())
+		})
+	})
+
+	t.Run("Prepare", func(t *testing.T) {
+		t.Run("Success", func(t *testing.T) {
+
+			mockService := new(MockService)
+			mockService.On("From").Return("from@example.com")
+			mockService.On("Expeditor").Return("Expeditor Name")
+			mockService.On("Send", mock.Anything).Return(nil)
+
+			mail := &mail.Mail{
+				To:          []string{"to@example.com"},
+				Cc:          []string{"cc@example.com"},
+				Bcc:         []string{"bcc@example.com"},
+				Subject:     "Test Subject",
+				Text:        []byte("This is a plain text body"),
+				Html:        []byte("<p>This is an HTML body</p>"),
+				Attachments: map[string][]byte{"test.txt": []byte("This is a test attachment")},
+			}
+
+			// Appel de la fonction à tester
+			msgBytes, recipients, err := mail.Prepare(mockService)
+			if err != nil {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+
+			// Vérification des résultats
+			if len(msgBytes) == 0 {
+				t.Errorf("Expected non-empty msgBytes")
+			}
+
+			expectedRecipients := []string{"to@example.com", "cc@example.com", "bcc@example.com"}
+			for i, recipient := range expectedRecipients {
+				if recipients[i] != recipient {
+					t.Errorf("Expected recipient %s, got %s", recipient, recipients[i])
+				}
+			}
+		})
+
+		t.Run("Failure", func(t *testing.T) {
+			m := &mail.Mail{
+				To:      []string{GOOD_EMAIL},
+				Subject: "Welcome to The Tip Top",
+			}
+
+			msg, to, err := m.Prepare(nil)
+			assert.Nil(t, msg)
+			assert.Nil(t, to)
+			assert.Error(t, err)
+		})
+	})
+
+}
