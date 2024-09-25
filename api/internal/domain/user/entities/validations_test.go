e4f2dec91e532555c553de04cc76f940d829c8ea
Florent <contact@making.codes>
2024-09-25 02:07:21 +0200

    fix ci

--- api/internal/domain/user/entities/validations_test.go
+++ api/internal/domain/user/entities/validations_test.go
@@ -3,6 +3,8 @@ package entities
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,5 +17,10 @@ func TestHasInClient(t *testing.T) {
 
 	assert.NotNil(t, vs.Has(PasswordRecover))
 	assert.Nil(t, vs.Has(MailValidation))
+
+	assert.Equal(t, v.IsPublic(), false)
+	assert.Equal(t, v.GetOwnerID(), v.CredentialID)
+	v.CredentialID = aws.String(uuid.New().String())
+	assert.Equal(t, v.GetOwnerID(), v.CredentialID)
 
 }
