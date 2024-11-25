3df3da918828af2986af6205223ed513b00cfddb
Kodflow <133899878+kodflow@users.noreply.github.com>
2024-11-25 21:42:48 +0100

    ok

--- diagram/src/thetiptop.py
+++ diagram/src/thetiptop.py
@@ -4,11 +4,10 @@ from diagrams.aws.database import RDS
 from diagrams.aws.network import NLB, Route53, VPC, CloudFront
 
 with Diagram("TheTipTop - Architecture", filename="architecture", show=False, outformat="png"):
-
     dns = Route53("DNS")
     cdn = CloudFront("CDN")
 
-    with Cluster("VPC"):  # Le VPC englobe tous les éléments internes
+    with Cluster("VPC"): 
         with Cluster("Private subnet"):
             lb = NLB("LoadBalancer")
             with Cluster("AutoScalingGroup"):
@@ -18,13 +17,12 @@ with Diagram("TheTipTop - Architecture", filename="architecture", show=False, ou
                            EC2("worker4"),
                            EC2("worker5")]
 
-        with Cluster("Isolated subnet"):  # Le sous-réseau isolé pour les bases de données
+        with Cluster("Isolated subnet"): 
             with Cluster("DBCluster"):
                 db_write = RDS("Databases Write")
                 db_read = [RDS("Databases Read")]
 
-    # Définition des flux
-    dns >> cdn >> lb  # CDN se connecte directement au LoadBalancer
-    lb >> workers  # LoadBalancer distribue le trafic vers les workers
-    workers >> db_write  # Les workers interagissent directement avec la base de données principale
-    db_write >> db_read  # La base de données Write réplique les données vers les Read
+    dns >> cdn >> lb
+    lb >> workers
+    workers >> db_write
+    db_write >> db_read 
