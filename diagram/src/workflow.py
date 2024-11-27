57b051c722e9c54c759cdc14322ce1db90c9a017
Kodflow <133899878+kodflow@users.noreply.github.com>
2024-11-28 00:03:25 +0100

    doc: add achitecture

--- diagram/src/workflow.py
+++
@@ -1,5 +0,0 @@
-from diagrams import Cluster, Diagram
-from diagrams.custom import Custom
-
-with Diagram("TheTipTop - Workflow", filename="workflow", show=False, outformat="png"):
-    Custom("Git", "./assets/github.svg")
